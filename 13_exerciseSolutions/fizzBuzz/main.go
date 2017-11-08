829cf40a3029e39dce045821fe1d9f447432b642
dineshauti <auti.dinesh3@gmail.com>
2017-11-08 18:36:03 -0500

    added module 13 exercise

--- 13_exerciseSolutions/fizzBuzz/main.go
+++ 13_exerciseSolutions/fizzBuzz/main.go
@@ -5,6 +5,13 @@ import "fmt"
 func main() {
 
 	for i := 0; i<100; i++ {
-		
+
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		}
 	}
 }
